cbdcd9efc7228dea90d8c34dc68cd0017e7bfa9b
ayorich <kayodele17@yahoo.com>
2020-01-07 11:47:12 +0100

    logo restructured

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-import MusicLogo from '../../assets/images/logo.png';
 import './Logo.css';
 
-const logo = () => <Link to="/" exact='true' >
-                     <img src={MusicLogo} alt="Logo" className="header__logo" />
-                    </Link>
+const logo = () => <span className="header__logo"><Link to="/" exact='true' >
+                      MINIMUSIC
+                    </Link></span>
                                         
 
 export default logo;
